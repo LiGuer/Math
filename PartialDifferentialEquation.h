4b4f315125dfbaa71f24fa2cb741110c3a5b0fec
liguer <2975395623@qq.com>
2020-11-23 22:45:55 +0800

    update

--- PartialDifferentialEquation.h
+++ PartialDifferentialEquation.h
@@ -1,6 +1,7 @@
 #include "Mat.h"
 /*----------------[ 波动方程 ]----------------
-*	[方程]: a ▽²u = ∂²a/∂t²
+*	[方程]: a ▽²u = ∂²u/∂t²
+			// ∂²u/∂t², 即.加速度
 			其中 ▽² ≡ ∂²/∂x² + ∂²/∂y² + ∂²/∂z² + ...
 		边界条件:
 			[1] u(r,t = 0) = f(r)		//初位置
@@ -10,41 +11,42 @@
 		u(t+1,...) = 2·u(t,...) - u(t-1,...)
 				+ Δt²·a{[u(x+1,...) - 2·u(x,...) + u(x-1,...)]/Δx² + ...}
 	[推导]:
-		∂²a/∂t² = { [u(t+1,rt) - u(t,rt)]/Δt - [u(t,rt) - u(t-1,rt)]/Δt } / Δt
+		∂²u/∂t² = { [u(t+1,rt) - u(t,rt)]/Δt - [u(t,rt) - u(t-1,rt)]/Δt } / Δt
 				= [u(t+1,rt) - 2·u(t,rt) + u(t-1,rt)] / Δt²
 		其他同理, 代入波动方程,
 		a { [u(x+1,...) - 2·u(x,...) + u(x-1,...)] / Δx² + ...}
-		= [u(t+1,...) - 2·u(t,...) + u(t-1,...)] / Δt²
+		  = [u(t+1,...) - 2·u(t,...) + u(t-1,...)] / Δt²
 		所以, 下一时刻有
 		u(t+1,...) = 2·u(t,...) - u(t-1,...)
-						+ Δt²·a{[u(x+1,...) - 2·u(x,...) + u(x-1,...)]/Δx² + ...}
+				+ Δt²·a{[u(x+1,...) - 2·u(x,...) + u(x-1,...)]/Δx² + ...}
 		对于t = 1 * Δt时刻, 有
-		u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + ▽²u·Δt²
-	[***] 该程序无法被使用，仅作为演示参考,只二维
+		u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + a▽²u·Δt²
+	[*] 暂时只二维
 **--------------------------------------*/
-void WaveEquation(Mat<double>& Map, Mat<double>& veloc, void (*setBoundaryEquations) (Mat<double>& x),
+void WaveEquation(Mat<double>& Map, Mat<double>& veloc, void (*setBoundaryEquations) (Mat<double>& x, int time),
 	double alpha, double deltaTime, double deltaX, double deltaY, int EndTimes) {
 	Mat<double> MapNow(Map), MapPrev(Map);
-	//对于t = 1 * Δt时刻, 有 u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + ▽²u·Δt²
-	for (int x = 0; x < Map.rows; x++) {
-		for (int y = 0; y < Map.cols; y++) {
+	//对于t = 1 * Δt时刻, 有 u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + a▽²u·Δt²
+	for (int x = 1; x < Map.rows - 1; x++) {
+		for (int y = 1; y < Map.cols - 1; y++) {
 			MapNow(x, y) += veloc(x, y) * deltaTime + alpha * deltaTime * deltaTime * (
-				(MapNow(x + 1, y) - 2 * MapNow(x, y) + MapNow(x - 1, y)) / (deltaX * deltaX)
-				+ (MapNow(x, y + 1) - 2 * MapNow(x, y) + MapNow(x, y - 1)) / (deltaY * deltaY)
+				(Map(x + 1, y) - 2 * Map(x, y) + Map(x - 1, y)) / (deltaX * deltaX)
+				+ (Map(x, y + 1) - 2 * Map(x, y) + Map(x, y - 1)) / (deltaY * deltaY)
 				);
 		}
-	}
-	while (--EndTimes) {//u(t+1,...) = 2·u(t,...) - u(t-1,...)+Δt²·a{ [u(x + 1,...) - 2·u(x,...) + u(x - 1,...)] / Δx² + ... }
-		for (int x = 0; x < Map.rows; x++) {
-			for (int y = 0; y < Map.cols; y++) {
-				MapPrev(x, y) =
-					2 * MapNow(x, y) - MapPrev(x, y) + alpha * deltaTime * deltaTime * (
+	}setBoundaryEquations(MapNow, 0);
+	for (int time = 1; time < EndTimes; time++) {
+		//u(t+1,...) = 2·u(t,...) - u(t-1,...)+Δt²·a{ [u(x + 1,...) - 2·u(x,...) + u(x - 1,...)] / Δx² + ... }
+		for (int x = 1; x < Map.rows - 1; x++) {
+			for (int y = 1; y < Map.cols - 1; y++) {
+				MapPrev(x, y) = 2 * MapNow(x, y) - MapPrev(x, y) + alpha * deltaTime * deltaTime * (
 					(MapNow(x + 1, y) - 2 * MapNow(x, y) + MapNow(x - 1, y)) / (deltaX * deltaX)
-						+ (MapNow(x, y + 1) - 2 * MapNow(x, y) + MapNow(x, y - 1)) / (deltaY * deltaY)
-						);
+					+ (MapNow(x, y + 1) - 2 * MapNow(x, y) + MapNow(x, y - 1)) / (deltaY * deltaY)
+					);
 			}
 		}
 		MapPrev.swap(MapNow);
-		setBoundaryEquations(MapNow);
+		setBoundaryEquations(MapNow, time);
 	}
+	Map = MapNow;
 }
