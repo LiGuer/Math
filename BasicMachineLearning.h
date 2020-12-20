d88f314d5e7f405f62a87d7c6f92afd5d8987e7f
liguer <2975395623@qq.com>
2020-12-20 15:53:28 +0800

    Update

--- BasicMachineLearning.h
+++ BasicMachineLearning.h
@@ -129,3 +129,57 @@ void PrincipalComponentsAnalysis(Mat<double>& x, Mat<double>& y, int yDim) {
 	//[5]
 	y.mult(W, x);
 }
+/*--------------------------------[ QLearning ]--------------------------------
+*	[定义]:Q(s,a) = (1 + lr)·Q(s,a) + lr·( R + g·max Q(s',:) )
+			s: state	a: action	R: reward	lr: learning rate	g: forget factor
+*	[原理]:
+		选择动作: ε-greedy方法: 
+			每个状态以ε概率随机选取动作，1-ε概率选择当前最优解
+		眼前利益R: 反馈值.
+		记忆中的利益 max Q(s',:): 小鸟记忆里，新位置[公式]能给出的最大效用值.
+		forget factor越大，小鸟就会越重视以往经验，越小，小鸟只重视眼前利益R.
+*	[流程]:
+		[1] Init Q table arbitrarily
+		[2] Repeat (for each episode), until s is terminal
+			[3] Choose a from s using policy derived from Q (eg. ε-greedy)
+			[4] Take action a, observe r s'
+			[5] Q(s,a) = (1 + lr)·Q(s,a) + lr·( R + g·max Q(s',:) )
+				s = s'
+*	[Ps]:
+		可以逐渐降低随机选取动作的概率ε，一开始随机率可达100%
+			然后随训练次数的深入，应当逐渐降低随机概率。
+-----------------------------------------------------------------------------*/
+class QLearning {
+public:
+	double learnRate = 0.6, Gamma = 0.8, greedy = 0.9; //奖励递减值# 贪婪度
+	int actionNum = 0, stateNum = 0;
+	Mat<double> QTable;
+	double preState = 0;
+	/*---------------- 初始化 ----------------*/
+	QLearning(int _stateNum, int _actionNum) { init(_stateNum, _actionNum); }
+	void init(int _stateNum, int _actionNum) {
+		actionNum = _actionNum;
+		stateNum = _stateNum;
+		QTable.zero(_stateNum, _actionNum);
+	}
+	/*---------------- 选择行为 ----------------*/
+	int chooseAction(int state) {
+		int action = 0;
+		bool flag = 1;
+		for (int i = 0; i < actionNum; i++)
+			if (QTable(state, i) != 0) { flag = 0; break; }
+		if (rand() / double(RAND_MAX) < greedy || flag) return rand() % actionNum;
+		double t = -DBL_MAX;
+		for (int i = 0; i < actionNum; i++)
+			if (QTable(state, i) > t) { t = QTable(state, i); action = i; }
+		return action;
+	}
+	/*---------------- 反馈学习 ----------------*/
+	void feedback(int state, int action, double R) {
+		double t = -DBL_MAX;
+		for (int i = 0; i < actionNum; i++)
+			t = QTable(state, i) > t ? QTable(state, i) : t;
+		QTable(preState, action) = (1 - learnRate) * QTable(preState, action) + learnRate * (R + Gamma * t);
+		preState = state;
+	}
+};
