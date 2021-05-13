d3a3df54cfee11a02fd207703614a62818826892
LiGuer <2975395623@qq.com>
2021-05-13 23:55:26 +0800

    Update

--- Statistics.h
+++ Statistics.h
@@ -17,39 +17,87 @@ limitations under the License.
 #include <vector>
 #include <algorithm>
 #define PI 3.141592653589
-
+/******************************************************************************
+*                    概率统计
+-------------------------------------------------------------------------------
+---------------- 基础统计特征 ----------------
+double Mean		(Mat<>& x);					//均值
+Mat<>& Mean		(Mat<>& x, Mat<>& ans);
+double Variance	(Mat<>& x);					//方差
+Mat<>& Variance	(Mat<>& x, Mat<>& ans);
+double NormalDistrib	(double x, double mean = 0, double var = 1);	//常见分布: 分布函数、密度函数
+double    ExpDensity	(double x, double mean);
+double    ExpDistrib	(double x, double mean);
+---------------- 假设检验 ----------------
+double	X2Test	(Mat<>& x, double St, double Ed, int N, F&& DistribFunc);	//X²拟合检验
+bool	SkewnessKurtosisTest	(Mat<>& x, double SignificanceLevel);		//偏度-峰度
+---------------- 其他 ----------------
+Mat<int>& Histogram(Mat<>& x, int N, Mat<int>& frequency, double overFlow, double underFlow);	//直方图
+void BoxPlot(Mat<>& x, Mat<>& MediQuartThreshold, std::vector<int>* OutlierIndex)	//箱形图
+******************************************************************************/
 namespace Statistics {
-	/*--------------------------------[ 基础参数 ]--------------------------------*/
-	double Mean(Mat<>& x);											//平均值
-	Mat<>& Mean(Mat<>& x, Mat<>& ans);								//平均值
-	double Variance(Mat<>& x);										//方差
-	Mat<>& Variance(Mat<>& x, Mat<>& ans);							//方差
-	/*--------------------------------[ 概率分布、检验 ]--------------------------------*/
-	bool SkewnessKurtosisTest(Mat<>& x, double SignificanceLevel);
-	/*--------------------------------[ 统计图表 ]--------------------------------*/
-	Mat<int>& Histogram(Mat<>& x, int N, Mat<int>& frequency, double overFlow = NULL, double underFlow = NULL);			//直方图
-	void BoxPlot(Mat<>& input, Mat<>& MediQuartThreshold, std::vector<int>* OutlierIndex);						//箱形图
+/*#############################################################################
+
+								基础统计特征
+
+#############################################################################*/
 /***************************************************************************
-*								常见分布  密度函数
+*								均值 / 方差
+*	[公式]:
+		Mean = Σx_i / N
+		Var  = Σ(x_i - x_mean)² / (N - 1)
+/***************************************************************************/
+//均值
+double Mean(Mat<>& x) { return x.sum() / x.size(); }
+Mat<>& Mean(Mat<>& x, Mat<>& ans) { return ans.mult(1.0 / x.cols, x.sum(ans, 1)); }
+//方差
+double Variance(Mat<>& x) {
+	double var = 0, mean = Mean(x);
+	for (int i = 0; i < x.size(); i++) var += pow(x[i] - mean, 2);
+	return var / (x.size() - 1);
+}
+Mat<>& Variance(Mat<>& x, Mat<>& ans) {
+	Mat<> mean; Mean(x, mean);
+	ans.alloc(x.rows);
+	for (int i = 0; i < x.rows; i++) {
+		for (int j = 0; j < x.cols; j++)
+			ans[i] += pow(x(i, j) - mean[i], 2);
+		ans[i] /= x.cols - 1;
+	} return ans;
+}
+/***************************************************************************
+*						常见分布: 分布函数、密度函数
 *	[公式]: 
 		正态分布: f(x) = 1 / sqrt(2πσ²)·exp(-(x-μ)² / (2σ²))
-		指数分布: f(x) = 1 / μ· exp(-x/μ)
+		指数分布: f(x) = 1 / μ· exp(-x/μ)	(x>0)
+				  F(x) = 1 - exp(-x/μ)			(x>0)
 /***************************************************************************/
-inline double NormalDistribFunc	(double x, double mean = 0, double var = 1) {
+inline double NormalDistrib	(double x, double mean = 0, double var = 1) {
 	return 1 / sqrt(2 * PI * var) * exp(-pow(x - mean, 2) / (2 * var));
 }
-inline double ExpDensityFunc	(double x, double mean) {
+inline double ExpDensity	(double x, double mean) {
 	return x <= 0 ? 0.0 : 1.0 / mean * exp(-x / mean);
 }
-inline double ExpDistribFunc	(double x, double mean) {
-	return x < 0 ? 0.0 : 1 - exp(-x / mean);
+inline double ExpDistrib	(double x, double mean) {
+	return x <= 0 ? 0.0 : 1 - exp(-x / mean);
 }
+/*#############################################################################
+
+								假设检验
+
+#############################################################################*/
 /***************************************************************************
 *								X²拟合检验
 *	[公式]: X² = Σ_(i=0)^N  fi² / (n pi) - n
 *	[流程]:
 		[1] 频数统计
 		[2] X²拟合检验
+----------------------------------------------------------------------------
+*	[Example]:
+		double mean = Statistics::Mean(data);
+		double X2   = Statistics::X2Test( data, 0, 20, 10,
+			[&mean](double x) { return Statistics::ExpDistrib(x, mean); }
+		);
 /***************************************************************************/
 template<typename F>
 double X2Test(Mat<>& x, double St, double Ed, int N, F&& DistribFunc) {
@@ -70,10 +118,117 @@ double X2Test(Mat<>& x, double St, double Ed, int N, F&& DistribFunc) {
 			(i == freq.size() - 1) ? (1 - DistribFunc(Ed)) :
 			DistribFunc(St + i * delta) - DistribFunc(St + (i - 1) * delta)
 			);
-		printf("[%f,%f] %d %f %f\n", St + (i - 1) * delta, St + i * delta, freq[i], n * p, p);
+		//printf("[%.0f,%.0f] %d %.3f %.3f\n", St + (i - 1) * delta, St + i * delta, freq[i], n * p, p);
 		if (p == 0) continue;
 		X2 += pow(freq[i], 2) / (n * p);
 	} return X2 -= n;
+}
+/***************************************************************************
+*								偏度峰度
+*	[定义]:
+		偏度: 样本标准化变量Y = [X - E(X)] / sqrt D(X)的三阶矩E(Y³)=E[(X-E(X))³]/D(X)³`²
+		峰度: 样本标准化变量Y = [X - E(X)] / sqrt D(X)的四阶矩E(Y⁴)=E[(X-E(X))⁴]/D(X)²
+*	[过程]:
+		[1] 计算样本中心矩 D(X), E[(X-E(X))³], E[(X-E(X))⁴]
+		[2] 计算偏度、峰度 E[(X-E(X))³]/D(X)³`², E[(X-E(X))⁴]/D(X)²
+/***************************************************************************/
+double Skewness(Mat<>& x) {
+	int N = x.cols;
+	double mean = Mean(x), B[5] = { 0 };
+	for (int k = 2; k <= 4; k++) {
+		for (int i = 0; i < N; i++) B[k] += pow(x[i] - mean, k);
+		B[k] /= N;
+	}
+	return B[3] / pow(B[2], 1.5);
+}
+double Kurtosis(Mat<>& x) {
+	int N = x.cols;
+	double mean = Mean(x), B[5] = { 0 };
+	for (int k = 2; k <= 4; k++) {
+		for (int i = 0; i < N; i++) B[k] += pow(x[i] - mean, k);
+		B[k] /= N;
+	}
+	return B[4] / pow(B[2], 2);
+}
+/***************************************************************************
+*								正态分布 - 偏度峰度检验
+*	[定义]:
+		偏度: 样本标准化变量Y = [X - E(X)] / sqrt D(X)的三阶矩E(Y³)=E[(X-E(X))³]/D(X)³`²
+		峰度: 样本标准化变量Y = [X - E(X)] / sqrt D(X)的四阶矩E(Y⁴)=E[(X-E(X))⁴]/D(X)²
+		若样本服从正态分布, 偏度 == 0 且 峰度 == 3
+*	[过程]:
+		[1] 计算样本中心矩 D(X), E[(X-E(X))³], E[(X-E(X))⁴]
+		[2] 计算偏度、峰度 E[(X-E(X))³]/D(X)³`², E[(X-E(X))⁴]/D(X)²
+		[3] 假设检验
+/***************************************************************************/
+bool SkewnessKurtosisTest(Mat<>& x, double SignificanceLevel) {
+	//[1]
+	int N = x.cols;
+	double mean = Mean(x), B[5] = { 0 };
+	for (int k = 2; k <= 4; k++) {
+		for (int i = 0; i < N; i++) B[k] += pow(x[i] - mean, k);
+		B[k] /= N;
+	}
+	//[2][3]
+	double 
+		Skewness = B[3] / pow(B[2], 1.5),
+		Kurtosis = B[4] / pow(B[2], 2),
+		sigma_1  = sqrt(6.0 * (N - 2) / (N + 1) / (N + 3)),
+		sigma_2  = sqrt(24.0 * N * (N - 2) * (N - 3) / (N + 1) / (N + 1) / (N + 3) / (N + 5)),
+		mu_2     = 3 - 6.0 / (N + 1),
+		u1		 = fabs(Skewness / sigma_1),
+		u2		 = fabs(Kurtosis - mu_2) / sigma_2;
+	if (u1 >= SignificanceLevel || u2 >= SignificanceLevel) return false;
+	return true;
+}
+/*#############################################################################
+
+								其他
+
+#############################################################################*/
+/***************************************************************************
+*								直方图
+*	[输入]: OverFlow: 上溢出	UnderFlow: 下溢出
+*	[注]: 区间规定"左开右闭"
+/***************************************************************************/
+Mat<int>& Histogram(Mat<>& x, int N, Mat<int>& frequency, double overFlow, double underFlow) {
+	double max = overFlow == NULL ? x.max() : overFlow,
+		min = underFlow == NULL ? x.min() : underFlow,
+		delta = (max - min) / N;
+	frequency.zero(1, N + (overFlow == NULL ? 1 : 0) + (underFlow == NULL ? 1 : 0));
+	for (int i = 0; i < x.cols; i++) {
+		int index = (x[i] - min) / delta + (overFlow == NULL ? 1 : 0);
+		index = x[i] <= min ? 0 : index;
+		index = x[i] > max ? frequency.cols - 1 : index;
+		frequency[index]++;
+	}
+	return frequency;
+}
+/***************************************************************************
+*								箱形图
+*[输出]: MediQuartThreshold: (1) 中位数 (2/3) 小/大四分位数 (4/5) 小/大边缘
+*[目的]: 数据 => 中位数、小/大四分位数  => 小/大边缘
+/***************************************************************************/
+void BoxPlot(Mat<>& x, Mat<>& MediQuartThreshold, std::vector<int>* OutlierIndex) {
+	// 中位数、小/大四分位数、小/大边缘
+	MediQuartThreshold.zero(x.rows, 5);
+	Mat<> xTmp(x);
+	for (int i = 0; i < x.rows; i++) {
+		std::sort(
+			xTmp.data + i	   * xTmp.cols,
+			xTmp.data +(i + 1) * xTmp.cols
+		);
+		MediQuartThreshold(i, 0) = (xTmp(i, 1.0 / 2 * xTmp.cols) + xTmp(i, (int)(1.0 / 2 * xTmp.cols + 0.5))) / 2;
+		MediQuartThreshold(i, 1) = (xTmp(i, 1.0 / 4 * xTmp.cols) + xTmp(i, (int)(1.0 / 4 * xTmp.cols + 0.5))) / 2;
+		MediQuartThreshold(i, 2) = (xTmp(i, 3.0 / 4 * xTmp.cols) + xTmp(i, (int)(3.0 / 4 * xTmp.cols + 0.5))) / 2;
+		double delta = MediQuartThreshold(i, 2) - MediQuartThreshold(i, 1);
+		MediQuartThreshold(i, 3) = MediQuartThreshold(i, 1) - 1.5 * delta;
+		MediQuartThreshold(i, 4) = MediQuartThreshold(i, 2) + 1.5 * delta;
+	}
+	for (int i = 0; i < x.rows; i++)
+		for (int j = 0; j < x.cols; j++)
+			if (x(i, j) < MediQuartThreshold(i, 3) || x(i, j) > MediQuartThreshold(i, 4))
+				OutlierIndex[i].push_back(j);
 }
 }
 #endif
