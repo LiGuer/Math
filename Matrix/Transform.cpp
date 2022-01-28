34fa60d78d5335b030aee7958e8ee8b7de609200
LiGuer <2975395623@qq.com>
2022-01-28 22:29:52 +0800

    Update

--- Matrix/Transform.cpp
+++ Matrix/Transform.cpp
@@ -1,19 +1,19 @@
 #include "Transform.h"
-/******************************************************************************
+/****************************************************************
 * 旋转变换
     * 初等旋转矩阵.
         $T_{ij} = [
-            1\\
-            & \ddots \\
-            & & 1\\
-            & & & & \cos\theta|_{(i,i)}& & & & \sin\theta|_{(i,j)} \\
-            & & & & & 1 &\\
-            & & & & & & \ddots\\
-            & & & & & & & 1\\
-            & & & &-\sin\theta|_{(j,i)}& & & & \cos\theta|_{(j,j)}\\
-            & & & & & & & & & 1 \\
-            & & & & & & & & & & \ddots \\
-            & & & & & & & & & & & 1 \\
+            1
+            & \ddots 
+            & & 1
+            & & & \cos\theta|_{(i,i)}& & & & \sin\theta|_{(i,j)} 
+            & & & & 1 &
+            & & & & & \ddots
+            & & & & & & 1
+            & & &-\sin\theta|_{(j,i)}& & & & \cos\theta|_{(j,j)}
+            & & & & & & & & 1 
+            & & & & & & & & & \ddots 
+            & & & & & & & & & & 1 
         ]$
     * 四元数
         v' = q v q`¹
@@ -32,34 +32,34 @@
                     |b  a  d -c|
                     |c -d  a  b|
                     [d  c -b  a]
-******************************************************************************/
+****************************************************************/
 // 初等旋转矩阵
-Mat<>& Matrix::rotate(int i, int j, double theta, Mat<>& transMat) {
-    transMat.E();
-    transMat(i, i) = transMat(j, j) = cos(theta);
-    transMat(i, j) = sin(theta);
-    transMat(j, i) =-sin(theta);
-    return transMat;
+Mat<>& Matrix::rotate(int i, int j, double theta, Mat<>& T) {
+    T.E();
+    T(i, i) = T(j, j) = cos(theta);
+    T(i, j) = sin(theta);
+    T(j, i) =-sin(theta);
+    return T;
 }
 
-Mat<>& Matrix::rotate(Mat<>& theta, Mat<>& transMat) {
-    transMat.E();
-	Mat<> rotateMat = transMat;
+Mat<>& Matrix::rotate (Mat<>& theta, Mat<>& T) {
+    T.E();
+	Mat<> rotateMat = T;
 
 	for (int i = 0; i < theta.rows; i++) {
 		for (int j = i + 1; j < theta.cols; j++) {
-			transMat.mul(
+			T.mul(
 				rotate(i, j, theta(i, j), rotateMat), 
-				transMat
+				T
 			);
 		}
 	}
-    return transMat;
+    return T;
 }
 
 //3D·四元数
-Mat<>& Matrix::rotate(Mat<>& rotateAxis, double theta, Mat<>& transMat) {
-	if (transMat.rows != 3 + 1) exit(-1);
+Mat<>& Matrix::rotate (Mat<>& rotateAxis, double theta, Mat<>& T) {
+	if (T.rows != 3 + 1) exit(-1);
 	
 	rotateAxis.normalize();
 	Mat<> q(4);				//四元数
@@ -70,47 +70,70 @@ Mat<>& Matrix::rotate(Mat<>& rotateAxis, double theta, Mat<>& transMat) {
 		sin(theta / 2) * rotateAxis[2]
 	};
 	// rotate mat
-	transMat.zero();
-	for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) transMat(i, j) = q[((j % 2 == 0 ? 1 : -1) * i + j + 4) % 4];
-	for (int i = 1; i < 4; i++) transMat(i, i % 3 + 1) *= -1;
-	Mat<> tmp = transMat;
+	T.zero();
+	for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) T(i, j) = q[((j % 2 == 0 ? 1 : -1) * i + j + 4) % 4];
+	for (int i = 1; i < 4; i++) T(i, i % 3 + 1) *= -1;
+	Mat<> tmp = T;
 	for (int i = 1; i < 4; i++) 
-		transMat(0, i) *= -1, 
+		T(0, i) *= -1, 
 		tmp     (i, 0) *= -1;
-	transMat *= tmp;
+	T *= tmp;
 
-	return transMat;
+	return T;
 }
 
-/******************************************************************************
+/****************************************************************
 * 反射变换
     $ y = H x = (I - 2 e_2 e_2^T) x $
-******************************************************************************/
-Mat<>& Matrix::reflect(Mat<>& e, Mat<>& transMat){
+****************************************************************/
+Mat<>& Matrix::reflect (Mat<>& e, Mat<>& T){
     Mat<> tmp;
-    transMat.mul(e, e.transpose(tmp));
-    transMat.sub(tmp.E(transMat.rows), transMat *= 2);
-    return transMat;
+    T.mul(e, e.transpose(tmp));
+    T.sub(tmp.E(T.rows), T *= 2);
+    return T;
 }
 
-/******************************************************************************
+/****************************************************************
+* 斜切变换
+    单位矩阵的第(i,j)个元素改为斜切比率 $a_{ij}$
+****************************************************************/
+Mat<>& shear (int i, int j, double a, Mat<>& T){
+    T.E();
+    T(i, j) = a;
+    return T;
+}
+
+/****************************************************************
 * 缩放变换
     $T_{ij} = [
-        dx_1 \\
-        & dx_2 \\
-        & & \ddots\\
+        dx_1 
+        & dx_2 
+        & & \ddots
         & & & dx_n
     ]$
-******************************************************************************/
-Mat<>& Matrix::scale(Mat<>& ratio, Mat<>& transMat) {
-    return transMat.diag(ratio);
+****************************************************************/
+Mat<>& Matrix::scale (Mat<>& ratio, Mat<>& T) {
+    return T.diag(ratio);
 }
 
-/******************************************************************************
+/****************************************************************
+* 正交投影
+    P_L = X(X^H X)^{-1}X^H
+    X = (x_1, ... , x_r)  投影后子空间的基
+****************************************************************/
+Mat<>& Matrix::project (Mat<>& X, Mat<>& T){
+    Mat<> tmp;
+    T.mul(X.transpose(tmp), X);
+    T.mul(X, T.inv(T);
+    T.mul(T, X.transpose(tmp));
+    return T;
+}
+
+/****************************************************************
 * 正交化
-******************************************************************************/
+****************************************************************/
 /*
-void orthogonalize(Mat<>& A) {
+void orthogonalize (Mat<>& A) {
 	Mat<> q;
 	q[0].normalize();
 	q[1] -= t1.mul(q[1].dot(q[0]), q[0]);   //施密特正交化
--- Matrix/Transform.h
+++ Matrix/Transform.h
@@ -5,13 +5,14 @@
  * 线性变换
  */
 namespace Matrix{
-Mat<>& rotate	(int i, int j, double theta, 		Mat<>& transMat);	// 旋转
-Mat<>& rotate	(Mat<>& theta, 						Mat<>& transMat);	// 旋转
-Mat<>& rotate	(Mat<>& rotateAxis, double theta, 	Mat<>& transMat);	// 旋转 (3D)
-Mat<>& scale	(Mat<>& ratio, 						Mat<>& transMat);	// 缩放
-Mat<>& reflect	(Mat<>& e, 							Mat<>& transMat);	// 镜像
-																		// 投影
-																		// 正交化
+Mat<>& rotate	(int i, int j, double theta, 		Mat<>& T);	// 旋转
+Mat<>& rotate	(Mat<>& theta, 						Mat<>& T);	// 旋转
+Mat<>& rotate	(Mat<>& rotateAxis, double theta, 	Mat<>& T);	// 旋转 (3D)
+Mat<>& shear	(int i, int j, double a, 			Mat<>& T);	// 斜切
+Mat<>& scale	(Mat<>& ratio, 						Mat<>& T);	// 缩放
+Mat<>& reflect	(Mat<>& e, 							Mat<>& T);	// 镜像
+Mat<>& project 	(Mat<>& X, 							Mat<>& T);	// 正交投影
+																// 正交化
 }
 
 #endif
