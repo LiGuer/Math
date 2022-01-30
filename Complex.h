d075120929a531638ad5b213dabd23614bad4826
LiGuer <2975395623@qq.com>
2022-01-30 18:33:14 +0800

    Update

--- Complex.h
+++ Complex.h
@@ -18,12 +18,6 @@ public:
 		return *this;
 	}
 
-	Complex& operator=(Complex a) {
-		real = a.real;
-		imag = a.imag;
-		return *this;
-	}
-
 	Complex& set(double _real, double _imag) {
 		real = _real;
 		imag = _imag;
@@ -58,9 +52,27 @@ Complex& I(Complex& a){
 /* 
  * 基础运算
  */
+ //模
+double abs(Complex& a) {
+	return sqrt(a.real * a.real + a.imag * a.imag);
+}
+
+//幅角
+double arg(Complex& a) {
+	return atan(a.imag / a.real);
+}
+
+//负
+Complex& operator-(Complex& a) {
+	Complex ans;
+
+	ans.real = -a.real;
+	ans.imag = -a.imag;
+	return ans;
+}
 
 //加
-Complex operator+(Complex& a, Complex& b) {
+Complex& operator+(Complex& a, Complex& b) {
 	Complex ans;
 
     ans.real = a.real + b.real;
@@ -68,7 +80,7 @@ Complex operator+(Complex& a, Complex& b) {
     return ans;
 }
 
-Complex operator+(Complex& a, double& b) {
+Complex& operator+(Complex& a, double b) {
 	Complex ans;
 
 	ans.real = a.real + b;
@@ -76,7 +88,7 @@ Complex operator+(Complex& a, double& b) {
 	return ans;
 }
 
-Complex operator+(double& a, Complex& b) {
+Complex& operator+(double a, Complex& b) {
 	Complex ans;
 
 	ans.real = b.real + a;
@@ -85,7 +97,7 @@ Complex operator+(double& a, Complex& b) {
 }
 
 //减
-Complex operator-(Complex& a, Complex& b) {
+Complex& operator-(Complex& a, Complex& b) {
 	Complex ans;
 
     ans.real = a.real - b.real;
@@ -93,7 +105,7 @@ Complex operator-(Complex& a, Complex& b) {
     return ans;
 }
 
-Complex operator-(Complex& a, double& b) {
+Complex& operator-(Complex& a, double b) {
 	Complex ans;
 
 	ans.real = a.real + b;
@@ -101,7 +113,7 @@ Complex operator-(Complex& a, double& b) {
 	return ans;
 }
 
-Complex operator-(double& a, Complex& b) {
+Complex& operator-(double a, Complex& b) {
 	Complex ans;
 
 	ans.real = a - b.real;
@@ -110,7 +122,7 @@ Complex operator-(double& a, Complex& b) {
 }
 
 //乘
-Complex operator*(Complex& a, Complex& b) {
+Complex& operator*(Complex& a, Complex b) {
 	Complex ans;
 
 	ans.real = a.real * b.real - a.imag * b.imag;
@@ -118,7 +130,7 @@ Complex operator*(Complex& a, Complex& b) {
     return ans;
 }
 
-Complex operator*(Complex& a, double& b) {
+Complex& operator*(Complex& a, double b) {
 	Complex ans;
 
 	ans.real = a.real * b;
@@ -126,7 +138,7 @@ Complex operator*(Complex& a, double& b) {
 	return ans;
 }
 
-Complex operator*(double& a, Complex& b) {
+Complex& operator*(double a, Complex& b) {
 	Complex ans;
 
 	ans.real = a * b.real;
@@ -134,7 +146,7 @@ Complex operator*(double& a, Complex& b) {
 	return ans;
 }
 //除
-Complex operator/(Complex& a, Complex& b) {
+Complex& operator/(Complex& a, Complex& b) {
 	Complex ans;
 
     double t =  b.real * b.real + b.imag * b.imag;
@@ -143,7 +155,7 @@ Complex operator/(Complex& a, Complex& b) {
     return ans;
 }
 
-Complex operator/(Complex& a, double& b) {
+Complex& operator/(Complex& a, double b) {
 	Complex ans;
 
 	ans.real = a.real / b;
@@ -151,7 +163,7 @@ Complex operator/(Complex& a, double& b) {
 	return ans;
 }
 
-Complex operator/(double& a, Complex& b) {
+Complex& operator/(double a, Complex& b) {
 	Complex ans;
 
 	double t = b.real * b.real + b.imag * b.imag;
@@ -161,20 +173,79 @@ Complex operator/(double& a, Complex& b) {
 }
 
 //共轭
-Complex& conjugate (Complex& a, Complex& ans){
+Complex& conj (Complex& a){
+	Complex ans;
+
     ans.real =  a.real;
     ans.imag = -a.imag;
     return ans;
 }
 
-//模
-double abs (Complex& a){
-    return sqrt(a.real * a.real + a.imag * a.imag);
+//幂  de Moiver公式
+Complex& Pow(Complex& a, double p) {
+	Complex ans;
+
+	double r = pow(abs(a), p), theta = p * arg(a);
+	ans.real = r * cos(theta);
+	ans.imag = r * sin(theta);
+	return ans;
 }
 
-//幅角
-double arg (Complex& a){
-    return atan(a.imag / a.real);
+Complex& Pow(Complex& a, Complex& p) {
+	Complex ans;
+
+	return ans;
+}
+
+//指数函数
+Complex& Exp(Complex& a) {
+	Complex ans;
+
+	ans.real = exp(a.real) * cos(a.imag);
+	ans.imag = exp(a.real) * sin(a.imag);
+	return ans;
+}
+
+//对数函数
+Complex& Ln(Complex& a) {
+	Complex ans;
+
+	ans.real = log(abs(a));
+	ans.imag = arg(a);
+	return ans;
+}
+
+//三角函数、双曲函数
+Complex& Cos(Complex& a) {
+	Complex ans, i;
+	I(i);
+
+	ans = (Exp(a * i) + Exp(-a * i)) / 2;
+	return ans;
+}
+
+Complex& Sin(Complex& a) {
+	Complex ans, i;
+	I(i);
+
+	ans = (Exp(a * i) - Exp(-a * i)) / (2 * i);
+	return ans;
+}
+
+Complex& Cosh(Complex& a) {
+	Complex ans, i;
+	I(i);
+
+	ans = (Exp(a) + Exp(-a)) / 2;
+	return ans;
+}
+
+Complex& Sinh(Complex& a) {
+	Complex ans, i;
+	I(i);
+
+	ans = (Exp(a) - Exp(-a)) / 2;
+	return ans;
 }
 
 }
